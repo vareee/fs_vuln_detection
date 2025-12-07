cc3610ebacdced0d070065528d5d5be14d773e38
vareee <tikhrv@gmail.com>
2025-12-08 00:37:43 +0300

    updated transcript_vuln logic, way of adding elements ot transcript and examples

--- PoC.py
+++ PoC.py
@@ -19,6 +19,7 @@ class ObjectCategory(Enum):
     Pubkey = "pubkey"
     Message = "message"
     Challenge = "challenge"
+    Response = "response"
 
 
 class TranscriptInspector:
@@ -34,6 +35,14 @@ class TranscriptInspector:
         def __init__(self, value, tid):
             self.value = value
             self.transcript_id = tid
+
+        @property
+        def x(self):
+            return self.value.x
+
+        @property
+        def y(self):
+            return self.value.y
 
         def _ensure_same_transcript_id(self, other):
             if isinstance(other, TranscriptInspector.TaggedValue) and self.transcript_id != other.transcript_id:
@@ -69,13 +78,20 @@ class TranscriptInspector:
         return TranscriptInspector.TaggedValue(value, self.transcript_id)
 
     # add object to transcript (who did it, its category (pubkey, challenge, ...), its number in transcript and round number)
-    def add(self, name: str, subject: str, category: ObjectCategory):
+    def add(self, name: str, subject: str, category: ObjectCategory, value):
         if name in self.elements:
             return
+        if category in (ObjectCategory.Commitment, ObjectCategory.Pubkey) and len(self.challenges) > 0:
+            raise TranscriptError(
+                            f"Element '{name}' (category={category.value}) was added after the first challenge "
+                            f"and was NOT included in that challenge."
+                        )
         
-        self.elements[name] = (subject, category, self.index, self.round)
+        tagged = self.tag(value)
+        self.elements[name] = (subject, category, self.index, self.round, tagged)
         self.index += 1
         self.challenges_mul[name] = set()
+        return tagged
 
     # func to imitate multiplication of objects (as in PoC there is no direct interaction between objects), used to detect errors in cross-round interaction
     def imitate_mul(self, object_name: str, challenge_name: str):
@@ -88,7 +104,7 @@ class TranscriptInspector:
         self.challenges_mul[object_name].add(challenge_name)
 
     #func to create a challenge (imitation of it)
-    def record_challenge(self, challenge_name: str, used_names: List[str]):
+    def record_challenge(self, challenge_name: str, used_names: List[str], value):
         used_set = set(used_names)
         for n in used_set:
             if n not in self.elements:
@@ -97,34 +113,14 @@ class TranscriptInspector:
                 )
 
         self.challenges[challenge_name] = used_set
-        self.add(challenge_name, subject="verifier", category=ObjectCategory.Challenge)
+        tagged = self.add(challenge_name, subject="verifier", category=ObjectCategory.Challenge, value=value)
         self.round += 1
-
-    # func to detect errors in transcript (if used unknown challenges or some public values were declared after challenges)
-    def analyze_verification(self, verification_used: List[str], challenge_names: List[str]):
-        for name in verification_used:
-            if name not in self.elements:
-                raise ValueError(f"Verification references unknown element '{name}'!") # error if some value was not declared it transcript
-
-            _, category, index, _ = self.elements[name]
-            if category in (ObjectCategory.Commitment, ObjectCategory.Pubkey):
-                for challenge_name in challenge_names:
-                    if challenge_name not in self.challenges:
-                        raise ValueError(f"Challenge '{challenge_name}' not recorded!") # error if some challenge was not declared it transcript
-                    
-                    # check if some public value (pubkey or commitment) was delcared after some challenge
-                    _, _, challenge_index, _ = self.elements[challenge_name]
-                    if index > challenge_index and name not in self.challenges[challenge_name]:
-                        raise TranscriptError(
-                            f"Element '{name}' (category={category.value}) "
-                            f"was added at index {index} after challenge '{challenge_name}' "
-                            f"(index {challenge_index}) but was NOT included in that challenge."
-                        )
+        return tagged
 
     # func to detect errors in cross-round interaction
     def check_cross_round_interaction(self, object1: str, object2: str):
-        _, _, _, round_1 = self.elements[object1]
-        _, _, _, round_2 = self.elements[object2]
+        _, _, _, round_1, _ = self.elements[object1]
+        _, _, _, round_2, _ = self.elements[object2]
 
         if round_1 == round_2:
             return
@@ -156,23 +152,30 @@ def i2b32(x: int) -> bytes:
 def serialize_point(point):
     return i2b32(point.value.x) + i2b32(point.value.y)
 
-def bad_verify(R, A, s, e):
-    LHS = s * G
-    RHS = R + e * A
-    return LHS.value == RHS.value
 
 # example of safe transcript
-transcript_safe = TranscriptInspector()
-
-transcript_safe.add("A1", "prover1", ObjectCategory.Pubkey)
-transcript_safe.add("R1", "prover1", ObjectCategory.Commitment)
-transcript_safe.add("A2", "prover2", ObjectCategory.Pubkey)
-transcript_safe.add("R2", "prover2", ObjectCategory.Commitment)
-transcript_safe.add("message", "verifier", ObjectCategory.Message)
-transcript_safe.record_challenge("e", used_names=["A1","A2","R1","R2","message"])
-
 try:
-    transcript_safe.analyze_verification(verification_used=["A1","A2","R1","R2","message"], challenge_names=["e"])
+    transcript_safe = TranscriptInspector()
+
+    a1 = random.randint(1, curve_order - 1)
+    A1 = transcript_safe.add(name="A1", subject="prover1", category=ObjectCategory.Pubkey, value=G*a1)
+
+    k1 = random.randint(1, curve_order - 1)
+    R1 = transcript_safe.add(name="R1", subject="prover1", category=ObjectCategory.Commitment, value=G*k1)
+    
+    a2 = random.randint(1, curve_order - 1)
+    A2 = transcript_safe.add(name="A2", subject="prover2", category=ObjectCategory.Pubkey, value=G*a2)
+
+    k2 = random.randint(1, curve_order - 1)
+    R2 = transcript_safe.add(name="R2", subject="prover2", category=ObjectCategory.Commitment, value=G*k2)
+
+    msg_rnd = random.randint(1, curve_order - 1)
+    msg = transcript_safe.add(name="message", subject="prover2", category=ObjectCategory.Message, value=G*msg_rnd)
+    
+    data = serialize_point(A1) + serialize_point(R1) + serialize_point(A2) + serialize_point(R2) + serialize_point(msg)
+    e = transcript_safe.record_challenge(challenge_name="e", used_names=["A1","A2","R1","R2","message"],
+                                    value=int.from_bytes(hashlib.sha256(data).digest(), "big")%curve_order)
+    
     print("No Fiat-Shamir heuristic vulnerability detected.")
 except TranscriptError as e:
     print("Detection:", e)
@@ -181,19 +184,32 @@ except TranscriptError as e:
 print("----------------")
 
 # example of transcript with TranscriptError
-transcript_vulnerability = TranscriptInspector()
-
-transcript_vulnerability.add(name="A1", subject="prover1", category=ObjectCategory.Pubkey)
-transcript_vulnerability.add(name="R1", subject="prover1", category=ObjectCategory.Commitment)
-transcript_vulnerability.record_challenge(challenge_name="c", used_names=["A1", "R1"])
-
-transcript_vulnerability.add(name="A2", subject="prover2", category=ObjectCategory.Pubkey)
-transcript_vulnerability.add(name="R2", subject="prover2", category=ObjectCategory.Commitment)
-transcript_vulnerability.add(name="msg", subject="verifier", category=ObjectCategory.Message)
-transcript_vulnerability.record_challenge(challenge_name="e", used_names=["A1", "R1", "A2", "R2", "msg"])
-
 try:
-    transcript_vulnerability.analyze_verification(verification_used=["A1", "A2", "R1", "R2", "msg"], challenge_names=["c", "e"])
+    transcript_vulnerability = TranscriptInspector()
+
+    a1 = random.randint(1, curve_order - 1)
+    A1 = transcript_vulnerability.add(name="A1", subject="prover1", category=ObjectCategory.Pubkey, value=G*a1)
+
+    k1 = random.randint(1, curve_order - 1)
+    R1 = transcript_vulnerability.add(name="R1", subject="prover1", category=ObjectCategory.Commitment, value=G*k1)
+
+    data = serialize_point(A1) + serialize_point(R1)
+    e1 = transcript_vulnerability.record_challenge(challenge_name="e1", used_names=["A1", "R1"],
+                                                value=int.from_bytes(hashlib.sha256(data).digest(), "big")%curve_order)
+
+    a2 = random.randint(1, curve_order - 1)
+    A2 = transcript_vulnerability.add(name="A2", subject="prover2", category=ObjectCategory.Pubkey, value=G*a2,)
+
+    k2 = random.randint(1, curve_order - 1)
+    R2 = transcript_vulnerability.add(name="R2", subject="prover2", category=ObjectCategory.Commitment, value=G*k2)
+
+    msg_rnd = random.randint(1, curve_order - 1)
+    msg = transcript_vulnerability.add(name="msg", subject="prover2", category=ObjectCategory.Message)
+
+    data = serialize_point(A1) + serialize_point(R1) + serialize_point(A2) + serialize_point(R2) + serialize_point(msg)
+    e2 = transcript_vulnerability.record_challenge(challenge_name="e2", used_names=["A1", "R1", "A2", "R2", "msg"],
+                                                value=int.from_bytes(hashlib.sha256(data).digest(), "big")%curve_order)
+
     print("No Fiat-Shamir heuristic vulnerability detected.")
 except TranscriptError as e:
     print("Detected:", e)
@@ -202,45 +218,45 @@ except TranscriptError as e:
 print("----------------")
 
 # example of cross transcript interaction with error
-transcript1 = TranscriptInspector()
-
-a1 = random.randint(1, curve_order - 1)
-A1 = transcript1.tag(G * a1)
-transcript1.add(name="A1", subject="prover", category=ObjectCategory.Pubkey)
-
-k1 = random.randint(1, curve_order - 1)
-R1 = transcript1.tag(G * k1)
-transcript1.add(name="R1", subject="prover", category=ObjectCategory.Commitment)
-
-data = serialize_point(R1) + serialize_point(A1) + message
-e1 = transcript1.tag(int.from_bytes(hashlib.sha256(data).digest(), "big") % curve_order)
-transcript1.record_challenge(challenge_name="e1", used_names=["R1", "A1"])
-
-s1 = transcript1.tag((k1 + e1.value * a1) % curve_order)
-transcript1.add(name="s1", subject="prover", category=ObjectCategory.Message)
-
-
-transcript2 = TranscriptInspector()
-
-a2 = random.randint(1, curve_order - 1)
-A2 = transcript2.tag(G * a2)
-transcript2.add("A2", "prover", ObjectCategory.Pubkey)
-
-k2 = random.randint(1, curve_order - 1)
-R2 = transcript2.tag(G * k2)
-transcript2.add("R2", "prover", ObjectCategory.Commitment)
-
-data = serialize_point(R2) + serialize_point(A2)
-e2 = transcript2.tag(int.from_bytes(hashlib.sha256(data).digest(), "big") % curve_order)
-transcript2.record_challenge(challenge_name="e2", used_names=["R2", "A2"])
-
-s2 = transcript2.tag((k2 + e2.value * a2) % curve_order)
-transcript2.add("s2", "prover", ObjectCategory.Message)
+def bad_verify(R, A, s, e):
+    LHS = s * G
+    RHS = R + e * A
+    return LHS.value == RHS.value
 
 try:
-    valid = bad_verify(R2, A2, s1, e1)
-    print("Verifier returned:", valid)
+    transcript1 = TranscriptInspector()
 
+    a1 = random.randint(1, curve_order - 1)
+    A1 = transcript1.add(name="A1", subject="prover", category=ObjectCategory.Pubkey, value=G*a1)
+
+    k1 = random.randint(1, curve_order - 1)
+    R1 = transcript1.add(name="R1", subject="prover", category=ObjectCategory.Commitment, value=G*k1)
+
+    data = serialize_point(R1) + serialize_point(A1) + message
+    e1 = transcript1.record_challenge(challenge_name="e1", used_names=["R1", "A1"], 
+                                      value=int.from_bytes(hashlib.sha256(data).digest(), "big")%curve_order)
+
+    s1 = transcript1.add(name="s1", subject="prover", category=ObjectCategory.Message, 
+                    value=(k1+e1.value*a1)%curve_order)
+
+
+    transcript2 = TranscriptInspector()
+
+    a2 = random.randint(1, curve_order - 1)
+    A2 = transcript2.add(name="A2", subject="prover", category=ObjectCategory.Pubkey, value=G*a2)
+
+    k2 = random.randint(1, curve_order - 1)
+    R2 = transcript2.add(name="R2", subject="prover", category=ObjectCategory.Commitment, value=G*k2)
+
+    data = serialize_point(R2) + serialize_point(A2)
+    e2 = transcript2.record_challenge(challenge_name="e2", used_names=["R2", "A2"], 
+                                 value=int.from_bytes(hashlib.sha256(data).digest(), "big")%curve_order)
+
+    s2 = transcript2.add(name="s2", subject="prover", category=ObjectCategory.Message, 
+                         value=(k2+e2.value*a2)%curve_order)
+
+    valid = bad_verify(R2, A2, s1, e1)
+    print("No Fiat-Shamir heuristic vulnerability detected.")
 except CrossTranscriptError as e:
     print("Detected:", e)
 
@@ -248,17 +264,33 @@ except CrossTranscriptError as e:
 print("----------------")
 
 # example of cross round object ineraction with error
-transcript_cross_round_vulnerability = TranscriptInspector()
-transcript_cross_round_vulnerability.add("A1", "prover1", ObjectCategory.Pubkey)
-transcript_cross_round_vulnerability.add("R1", "prover1", ObjectCategory.Commitment)
-transcript_cross_round_vulnerability.record_challenge("e1", ["A1", "R1"])
-
-transcript_cross_round_vulnerability.add("A2", "prover2", ObjectCategory.Pubkey)
-transcript_cross_round_vulnerability.add("R2", "prover2", ObjectCategory.Commitment)
-transcript_cross_round_vulnerability.record_challenge("e2", ["A2", "R2"])
-
 try:
-    transcript_cross_round_vulnerability.check_cross_round_interaction("A1", "A2") 
+    transcript_round_vuln = TranscriptInspector()
+
+    msg_rnd = random.randint(1, curve_order - 1)
+    msg = transcript_round_vuln.add(name="msg", subject="prover", category=ObjectCategory.Message, value=G*msg_rnd)
+
+    a = random.randint(1, curve_order - 1)
+    A = transcript_round_vuln.add(name="A", subject="prover", category=ObjectCategory.Pubkey, value=G*a)
+
+    k = random.randint(1, curve_order - 1)
+    R = transcript_round_vuln.add(name="R", subject="prover", category=ObjectCategory.Commitment, value=G*k)
+    
+    data = serialize_point(R) + serialize_point(A)
+    e1 = transcript_round_vuln.record_challenge(challenge_name="e1", used_names=["A", "R"], 
+                                        value=int.from_bytes(hashlib.sha256(data).digest(), "big")%curve_order)
+
+    Z1 = transcript_round_vuln.add(name="Z1", subject="prover", category=ObjectCategory.Response, 
+                                   value=A-e1*msg)
+
+    data = serialize_point(R) + serialize_point(A) + serialize_point(Z1)
+    e2 = transcript_round_vuln.record_challenge(challenge_name="e2", used_names=["A", "R", "Z1"],
+                                           value=int.from_bytes(hashlib.sha256(data).digest(), "big")%curve_order)
+    
+    transcript_round_vuln.add(name="Z2", subject="prover", category=ObjectCategory.Response, 
+                              value=A-e2*msg)
+    
+    transcript_round_vuln.check_cross_round_interaction("Z2", "A")
     print("No Fiat-Shamir heuristic vulnerability detected.")
 except CrossRoundError as e:
     print("Detected:", e)
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## About
 
-This project is aimed to create mechanism whicn can be used to find vulnerabilities in Fiat-Shamir transfromation implementations.
+This project is aimed to create mechanism which can be used to find vulnerabilities in Fiat-Shamir transfromation implementations.
